db8d7b4d22b8183e72151a936b9300f8cbc0b5b8
ndv <nicolas.divittorio@legal-i.ch>
2021-12-21 19:50:46 +0100

    added tailwindcss

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,7 +6,11 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@500;700&display=swap"
             rel="stylesheet"
