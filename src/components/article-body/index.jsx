ccb0f1b26fe3674d108b85d6c02da9b40f5abd7e
ndv <nicolas.divittorio@legal-i.ch>
2021-12-21 11:13:36 +0100

    removed component

--- src/components/article-body/index.jsx
+++ src/components/article-body/index.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import Button from "components/button";
 import { MDXRemote } from "next-mdx-remote";
 import { Chart } from "react-google-charts";
 
@@ -7,7 +6,6 @@ import styles from "./article-body.module.css";
 
 // load components to pass to MDX
 const components = {
-  Button,
   Chart,
 };
 
