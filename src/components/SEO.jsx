dc754561df7391e7441fa27471bc00db59a99ffa
ndv <nicolas.divittorio@legal-i.ch>
2021-12-26 21:35:31 +0100

    added missing head wrapper tag

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,4 +1,5 @@
 import config from "config.js";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 function SEO({
@@ -10,22 +11,24 @@ function SEO({
   const router = useRouter();
   return (
     <>
-      <title>{title}</title>
-      <meta name="robots" content="follow, index" />
-      <meta name="description" content={description} />
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:url" content={`${config.siteUrl}${router.asPath}`} />
-      <meta property="og:type" content={ogType} />
-      <meta property="og:site_name" content={config.siteName} />
-      <meta property="og:description" content={description} />
-      <meta property="og:title" content={title} />
-      {typeof ogImage === "string" ? (
-        <meta property="og:image" content={ogImage} key={ogImage} />
-      ) : (
-        ogImage.map(({ url }) => (
-          <meta property="og:image" content={url} key={url} />
-        ))
-      )}
+      <Head>
+        <title>{title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={description} />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:url" content={`${config.siteUrl}${router.asPath}`} />
+        <meta property="og:type" content={ogType} />
+        <meta property="og:site_name" content={config.siteName} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        {typeof ogImage === "string" ? (
+          <meta property="og:image" content={ogImage} key={ogImage} />
+        ) : (
+          ogImage.map(({ url }) => (
+            <meta property="og:image" content={url} key={url} />
+          ))
+        )}
+      </Head>
     </>
   );
 }
