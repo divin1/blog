47bdcd5b2215d12131714fa9c09ee3c0bd1f72a8
ndv <nicolas.divittorio@legal-i.ch>
2021-12-26 21:16:08 +0100

    from json conf to js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "tabWidth": 2,
+    "printWidth": 80,
+    "endOfLine": "auto",
+    "arrowParents": "avoid",
+    "trailingComma": "es5",
+    "semi": true,
+    "useTabs": false,
+    "singleQuote": false
+}
