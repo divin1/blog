72a3dd17f928e1f83788cccb5090e751827e9387
divin1 <divicolas@gmail.com>
2021-12-29 19:25:11 +0100

    fixed text and url hover effect

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,15 +12,15 @@ function About() {
           <p className="pt-16">
             I was born in 1996, grew up and still live in Switzerland. <br />I
             studied Computer Science and, right after university, I joined a
-            young and ambitious startup on a mission to revulutionize the
+            young and ambitious startup on a mission to revolutionize the
             processes of one of the most stagnant business fields.
           </p>
           <p className="pt-8">
             I like to learn, understand and simplify things. <br />
-            The goal of this blog is to expose thoughts, ideas and processes,
-            trasparently, throughout my journey. You will find articles on
-            anything that intersects with the modern human condition, and the
-            utmost goal of self actualization:
+            The goal of this blog is to expose my thoughts, ideas and processes,
+            trasparently and throughout my journey. <br />
+            You will find articles on anything that intersects with the modern
+            human condition, and the utmost goal of self actualization:
           </p>
           <ul className="p-4 list-disc list-inside">
             <li>Entrepreneurship</li>
@@ -39,7 +39,10 @@ function About() {
           </h2>
           <p>
             Feel free to send me an email at{" "}
-            <a href={`mailto:${config.email}`}>
+            <a
+              href={`mailto:${config.email}`}
+              className="text-stone-500 dark:text-stone-300 hover:text-secondary-400 dark:hover:text-secondary-400"
+            >
               {config.email.replace("@", " [at] ")}
             </a>
             .
