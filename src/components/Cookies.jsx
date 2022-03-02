02b3570f4a75b2a611e7c1ab246f07e2719dc642
divin1 <divicolas@gmail.com>
2022-03-02 13:25:04 +0100

    fixed dialog staying open on route change

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Dialog } from "@headlessui/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 function Cookies() {
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const initialState = localStorage.getItem("cookies-notice") === null;
     setIsOpen(initialState);
   }, []);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  }, [router.asPath]);
 
   function close() {
     localStorage.setItem("cookies-notice", true);
@@ -20,10 +28,10 @@ function Cookies() {
     <Dialog
       open={isOpen}
       onClose={() => close()}
-      className="fixed inset-0 z-10 overflow-y-auto"
+      className="z-25 fixed inset-0 overflow-y-auto"
     >
       <div className="flex min-h-screen items-center justify-center">
-        <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+        <Dialog.Overlay className="z-21 fixed inset-0 bg-black opacity-30" />
         <div className="relative mx-auto max-w-sm rounded rounded-xl bg-white p-4 shadow-md dark:bg-stone-900">
           <Dialog.Title className="text-center">
             <span className="animate-bounce">🍪</span>
@@ -40,9 +48,9 @@ function Cookies() {
             </a>{" "}
             to enable analytics to monitor activity.
             <br />
-            <Link href="articles/transparency" passHref>
+            <Link href="/articles/transparency" passHref>
               <a className="text-stone-500 underline hover:text-secondary-400 dark:text-stone-300 dark:hover:text-secondary-400">
-                Here's my full statement
+                More on Data &#38; Privacy.
               </a>
             </Link>
           </Dialog.Description>
