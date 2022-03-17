cae015d2369175f1aafe91301e31aba358df006d
divin1 <divicolas@gmail.com>
2022-03-17 14:38:47 +0100

    refactor to analytics service

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,11 +1,27 @@
 import keys from "keys";
 
-export function pageView(url) {
-  window.gtag("config", keys.GOOGLE_ANALYTICS, {
-    page_path: url,
-  });
+class Analytics {
+  constructor() {
+    this.enabled = false;
+  }
+
+  init(localStorage) {
+    const storedPreference = localStorage.getItem("analytics-enabled");
+    this.enabled =
+      storedPreference === null ? false : JSON.parse(storedPreference);
+  }
+
+  pageView(url) {
+    if (!this.enabled) return;
+    window.gtag("config", keys.GOOGLE_ANALYTICS, {
+      page_path: url,
+    });
+  }
+
+  event({ action, params }) {
+    if (!this.enabled) return;
+    window.gtag("event", action, params);
+  }
 }
 
-export function event({ action, params }) {
-  window.gtag("event", action, params);
-}
+export default new Analytics();
