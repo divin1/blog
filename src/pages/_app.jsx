9e344218ad7cd480d637e34ba4112d39232ca3c5
divin1 <divicolas@gmail.com>
2022-08-24 10:52:29 +0200

    remove cookies notice

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import "styles/main.scss";
 
-import Cookies from "components/Cookies";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system">
-        <Cookies />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
