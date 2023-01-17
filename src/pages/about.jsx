39bb46c05f6b0c4b95b0f78d5627fc512edea09c
divin1 <divicolas@gmail.com>
2023-01-17 13:41:06 +0100

    minor improvements

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,31 +9,25 @@ function About() {
       <SEO />
       <Layout>
         <Container>
-          <p className="pt-16">
-            I'm Nicolas. I was born in 1996 and I'm from Switzerland. <br />I
-            studied Computer Science and, right after university, I joined a
-            young and ambitious startup on a mission to revolutionize the
-            processes of one of the most stagnant business fields.
-          </p>
-          <p className="pt-8">
-            I like to learn, understand and simplify things. <br />
-            The goal of this blog is to trasparently expose what I learn, my
-            thoughts, ideas and processes throughout my journey. <br />
-          </p>
-          <p>
-            Hope you enjoy your stay.
+          <p className="pt-24">
+            Hi, I'm Nicolas. <br />
+            I was born in 1996 and I'm from Switzerland. <br />I studied
+            Computer Science and, right after university, I joined a young and
+            ambitious startup on a mission to revolutionize the processes of one
+            of the most stagnant business fields.
             <br />
-            <b>Nicolas</b>
+            I like to learn, understand and simplify things. <br />
+            The goal of this blog is to expose thoughts and ideas that I develop
+            or come across throughout my journey.
           </p>
-          <h2 className="mt-24 text-xl font-bold text-stone-900 dark:text-stone-50">
-            Contact
-          </h2>
-          <p>
-            Feel free to send me a message at{" "}
+          <p className="pt-16">
+            Got anything to say or ask?
+            <br /> Feel free to send me a message at{" "}
             <a
               href={`https://twitter.com/${config.twitter}`}
               target="_blank"
               className="text-stone-500 hover:text-secondary-400 dark:text-stone-300 dark:hover:text-secondary-400"
+              rel="noreferrer"
             >
               @{config.twitter}
             </a>
