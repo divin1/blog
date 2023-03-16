230025915ff1dcbd9f5ba151e9c75f6449e71b1c
divin1 <divicolas@gmail.com>
2023-03-16 19:45:04 +0100

    add vercel analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import "styles/main.scss";
 
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <ThemeProvider attribute="class" defaultTheme="system">
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </>
   );
