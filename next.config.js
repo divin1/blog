318c2c35b9614a4debd78cd46ea425d78b563daf
Nicolas <divicolas@gmail.com>
2025-05-08 23:32:13 +0200

    chore: cleanup

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const nextConfig = {
   swcMinify: true,
-  // Add other Next.js configurations here if needed
 };
 
-// Apply the plugin directly to the nextConfig
 module.exports = withBundleAnalyzer(nextConfig);
