961a59279c8ba5cbb5321f2e8b7a1ba4c80d6631
Nicolas <divicolas@gmail.com>
2025-05-08 16:46:40 +0200

    chore: minor improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,36 @@
-import { GitHub, Rss } from "react-feather";
+import config from "config";
+import { GitHub, Linkedin, Rss } from "react-feather";
 
 function Footer() {
   return (
     <footer className="min-h-32 flex w-full flex-col items-center space-y-4 p-12 text-base dark:bg-stone-900">
-      <span>ndv {new Date().getFullYear()}</span>
-
       <div className="flex items-center space-x-8">
-        <a href="/rss.xml" target="_blank" aria-label="RSS">
-          <Rss />
-        </a>
         <a
-          href="https://github.com/divin1"
+          href={config.github}
           target="_blank"
           aria-label="GitHub"
           rel="noreferrer"
         >
-          <GitHub />
+          <GitHub className="h-4 w-4 transition-colors delay-150 duration-300 hover:text-primary-500" />
+        </a>
+
+        <a
+          href={config.linkedin}
+          target="_blank"
+          aria-label="LinkedIn"
+          rel="noreferrer"
+        >
+          <Linkedin className="h-4 w-4 transition-colors delay-150 duration-300 hover:text-primary-500" />
+        </a>
+
+        <a href="/rss.xml" target="_blank" aria-label="RSS">
+          <Rss className="h-4 w-4 transition-colors delay-150 duration-300 hover:text-primary-500" />
         </a>
       </div>
+
+      <span className="text-[13px] text-gray-400">
+        All rights reserved © {config.fullName} {new Date().getFullYear()}
+      </span>
     </footer>
   );
 }
