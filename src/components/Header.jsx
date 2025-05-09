0a03271a7b8fafe222c1a7135729b78c2026bfad
Nicolas <divicolas@gmail.com>
2025-05-09 15:19:27 +0200

    feat: improve navigation ft. new nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import clsx from "clsx";
+import NavMenu from "components/NavMenu";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -19,35 +20,26 @@ function isScrollTop() {
   return isTop;
 }
 
-const Header = () => {
+function Header() {
   const isTop = isScrollTop();
 
   return (
     <header
       className={clsx({
-        "firefox:bg-opacity-100 dark:firefox:bg-opacity-100 sticky top-0 z-20 w-full bg-stone-100 bg-opacity-30 backdrop-blur-sm backdrop-saturate-150 backdrop-filter dark:bg-stone-900 dark:bg-opacity-30": true,
+        "firefox:bg-surface/100 dark:firefox:bg-surface/100 sticky top-0 z-20 w-full bg-stone-100 bg-stone/30 backdrop-blur-sm backdrop-saturate-150 backdrop-filter dark:bg-stone-900 dark:bg-stone/30": true,
         "border-none": isTop,
         "border-b border-stone-200 dark:border-stone-800": !isTop,
       })}
     >
       <nav className="flex items-center space-x-4 p-6">
         <Link href="/" passHref>
-          <div className="h-8 w-8 cursor-pointer rounded-full bg-gradient-to-r from-rose-400 to-indigo-500 hover:shadow-lg hover:shadow-indigo-500/20" />
+          <div className="h-8 w-8 cursor-pointer rounded-full bg-linear-90 from-indigo-500 via-purple-500 to-pink-500 bg-gradient-to-r hover:shadow-lg hover:shadow-indigo-500/20" />
         </Link>
         <div className="grow" />
-        <Link href="/" passHref>
-          <a className="text-stone-500 hover:text-secondary-400 dark:text-stone-300 dark:hover:text-secondary-400">
-            Articles
-          </a>
-        </Link>
-        <Link href="/about" passHref>
-          <a className="text-stone-500 hover:text-secondary-400 dark:text-stone-300 dark:hover:text-secondary-400">
-            About
-          </a>
-        </Link>
+        <NavMenu />
       </nav>
     </header>
   );
-};
+}
 
 export default Header;
