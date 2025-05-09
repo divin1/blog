dbbad5c4910d2bc4a4664d7e81d0f1c9cd4601b4
Nicolas <divicolas@gmail.com>
2025-05-09 11:51:36 +0200

    fix: plugins import typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,5 @@ module.exports = {
       },
     }
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms", require("@tailwindcss/postcss")),
-],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/postcss")],
 };
